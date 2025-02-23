65b87c66387d10713db2999adc14675cd7158892
sanjeev <60956846+sanjeev@users.noreply.github.com>
2025-02-23 15:17:54 +0530

    Adding file for test purposes only.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,16 +35,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css"
-        />
-        <link
-          rel="stylesheet"
-          type="text/css"
-          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css"
-        /> */}
         <link
           href="https://fonts.googleapis.com/css?family=Nunito:400,700&display=swap"
           rel="stylesheet"
