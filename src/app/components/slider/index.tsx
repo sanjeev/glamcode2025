cf7cf775b6891450f2e4c40a3ecc78e07e2fc34f
sanjeev <60956846+sanjeev@users.noreply.github.com>
2025-02-27 10:29:52 +0530

    Adding file 26 feb2025

--- src/app/components/slider/index.tsx
+++ src/app/components/slider/index.tsx
@@ -1,4 +1,5 @@
 "use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 
@@ -9,6 +10,7 @@ const ReactSlider = dynamic(() => import("react-slick"), {
 });
 
 export default function slideSlick({ content }: any) {
+  const [isOpen, setIsOpen] = useState(false);
   const settings = {
     className: "home-slide ",
     arrows: true,
@@ -22,54 +24,72 @@ export default function slideSlick({ content }: any) {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsOpen(true);
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:grid-cols-2">
-      <div className="hidden md:block  lg:block">
-        <div className="pt-[150px]">
-          <Image
-            // loader={externaImageLoader}
-            src="/images/glam.png"
-            width={80}
-            height={80}
-            alt="Glam code"
-            priority
-          />
-          <p
-            style={{
-              fontSize: "2.5rem",
-              fontWeight: 700,
-              lineHeight: 1.2,
-            }}
-          >
-            Salon at Home for Women
-          </p>
-          <p className="spanp">⭐ 4.76(978k)</p>
+    <>
+      {isOpen === false ? (
+        <div className="overlay">adsasdasd</div>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 sm:grid-cols-2">
+          <div className="hidden md:block  lg:block">
+            <div className="pt-[150px]">
+              <Image
+                // loader={externaImageLoader}
+                src="/images/glam.png"
+                width={80}
+                height={80}
+                alt="Glam code"
+                priority
+              />
+              <p
+                style={{
+                  fontSize: "2.5rem",
+                  fontWeight: 700,
+                  lineHeight: 1.2,
+                }}
+              >
+                Salon at Home for Women
+              </p>
+              <p className="spanp">⭐ 4.76(978k)</p>
+            </div>
+          </div>
+          <div>
+            <ReactSlider {...settings}>
+              {content?.map((slide: any, index: number) => {
+                return (
+                  <div
+                    key={index}
+                    className="md:h-[440px] h-48 w-full  relative"
+                  >
+                    <Image
+                      src={slide?.slider_image_base_url}
+                      alt="Glam code"
+                      placeholder="blur"
+                      blurDataURL={slide?.slider_image_base_url}
+                      fill
+                      quality={10}
+                      loading="eager"
+                      style={{
+                        objectFit: "cover",
+                      }}
+                      className="rounded-lg"
+                      priority={true}
+                    />
+                  </div>
+                );
+              })}
+            </ReactSlider>
+          </div>
         </div>
-      </div>
-      <div>
-        <ReactSlider {...settings}>
-          {content?.map((slide: any, index: number) => {
-            return (
-              <div key={index} className="md:h-[440px] h-48 w-full  relative">
-                <Image
-                  src={slide?.slider_image_base_url}
-                  alt="Glam code"
-                  placeholder="blur"
-                  blurDataURL={slide?.slider_image_base_url}
-                  fill
-                  quality={10}
-                  loading="eager"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                  className="rounded-lg"
-                  priority={true}
-                />
-              </div>
-            );
-          })}
-        </ReactSlider>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
